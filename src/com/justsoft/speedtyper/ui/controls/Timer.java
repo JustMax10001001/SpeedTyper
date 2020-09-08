ad3d698f2db6c64c12f5ee0a6652c8c6c683b044
Max <rudko.max10@gmail.com>
2020-09-08 19:57:33 +0300

    Clean up Timer.java

--- src/com/justsoft/speedtyper/ui/controls/Timer.java
+++ src/com/justsoft/speedtyper/ui/controls/Timer.java
@@ -2,6 +2,10 @@ package com.justsoft.speedtyper.ui.controls;
 
 import com.justsoft.speedtyper.ui.dialogs.ExceptionDialog;
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.ReadOnlyBooleanWrapper;
 import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.concurrent.Service;
@@ -12,10 +16,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Timer extends VBox {
 
@@ -25,13 +26,19 @@ public class Timer extends VBox {
     private Label timeLabel;
 
     private final ReadOnlyIntegerWrapper timeRemainingProperty = new ReadOnlyIntegerWrapper(DEFAULT_TIMER_VALUE);
+    private final ReadOnlyBooleanWrapper isPausedProperty = new ReadOnlyBooleanWrapper(false);
+    private final ReadOnlyBooleanWrapper isRunningProperty = new ReadOnlyBooleanWrapper(false);
+
+    private final ScheduledExecutorService timerTaskExecutor = Executors.newSingleThreadScheduledExecutor();
 
     @FXML
     private int timerLength = DEFAULT_TIMER_VALUE;
 
-    private volatile boolean paused = false;
+    //private volatile boolean paused = false;
 
-    private Service<Void> timerService;
+    private ScheduledFuture<?> timerScheduleHandle;
+
+    private TimerFinishedEventHandler onFinishedEventHandler;
 
     public Timer() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/justsoft/speedtyper/resources/controls/timer_control.fxml"));
@@ -48,98 +55,64 @@ public class Timer extends VBox {
     @FXML
     private void initialize() {
         this.getStyleClass().add("timer");
-        timerService = constructTimerService();
 
-        timerService.setOnFailed(event -> {
-            timerService.getException().printStackTrace();
-            timeRemainingProperty.set(timerLength);
-        });
-        timerService.setOnCancelled(event -> {
-            System.out.println("Timer cancelled");
-            timeRemainingProperty.set(timerLength);
-        });
-        timerService.setOnSucceeded(event -> timeRemainingProperty.set(timerLength));
-        timeRemainingProperty.addListener((observable, oldValue, newValue) -> {
-            updateLabel(newValue.intValue());
-        });
+        timeLabel.textProperty().bind(
+                Bindings.createStringBinding(
+                        () -> String.format("%02d:%02d", timeRemainingProperty.get() / 60, timeRemainingProperty.get() % 60),
+                        timeRemainingProperty
+                )
+        );
+    }
+
+    private void timerTick() {
+        if (isPausedProperty.get())
+            return;
+
+        if (timeRemainingProperty.get() > 0) {
+            Platform.runLater(() ->
+                timeRemainingProperty.set(timeRemainingProperty.get() - 1)
+            );
+        } else {
+            if (onFinishedEventHandler != null)
+                Platform.runLater(() -> {
+                    isRunningProperty.set(false);
+                    isPausedProperty.set(false);
+                    onFinishedEventHandler.onFinished();
+                });
+            cancel();
+        }
     }
 
     public void start() {
-        if (timerService.isRunning())
-            throw new IllegalStateException("Timer is already running");
+        timeRemainingProperty.set(timerLength);
+        isRunningProperty.set(true);
+        isPausedProperty.set(false);
 
-        timerService.reset();
-        timerService.start();
+        timerScheduleHandle = timerTaskExecutor.scheduleAtFixedRate(this::timerTick, 0, 1, TimeUnit.SECONDS);
     }
 
     public boolean cancel() {
-        return timerService.cancel();
-    }
-
-    public void resume() {
-        paused = false;
-    }
-
-    public void pause() {
-        if (!timerService.isRunning())
-            throw new IllegalStateException("Timer is not running");
-        paused = true;
+        return timerScheduleHandle.cancel(false);
     }
 
     public void setOnFinished(TimerFinishedEventHandler handler) {
-        timerService.setOnSucceeded(event -> {
-            handler.onFinished();
-            timeRemainingProperty.set(timerLength);
-        });
-    }
-
-    private void updateLabel(int newValue) {
-        Platform.runLater(() -> timeLabel.setText(String.format("%02d:%02d", newValue / 60, newValue % 60)));
-    }
-
-    private Service<Void> constructTimerService() {
-        return new Service<Void>() {
-            @Override
-            protected Task<Void> createTask() {
-                return constructTimerTask();
-            }
-        };
-    }
-
-
-    private Task<Void> constructTimerTask() {
-        return new Task<Void>() {
-            @Override
-            protected Void call() throws Exception {
-                timeRemainingProperty.set(timerLength);
-                paused = false;
-                while (timeRemainingProperty.get() > 0 && !this.isCancelled()) {
-                    if (!paused) {
-                        timeRemainingProperty.set(timeRemainingProperty.get() - 1);
-                        Thread.sleep(1000);
-                    } else {
-                        Thread.sleep(0, 10000);
-                    }
-                }
-                return null;
-            }
-        };
+        this.onFinishedEventHandler = handler;
     }
 
     public ReadOnlyIntegerProperty timeRemainingProperty() {
         return timeRemainingProperty.getReadOnlyProperty();
     }
 
-    /**
-     * Sets timer length
-     *
-     * @param timerLength - timer delay in seconds
-     */
+    public ReadOnlyBooleanProperty isRunningProperty() {
+        return isRunningProperty.getReadOnlyProperty();
+    }
+
+    public ReadOnlyBooleanProperty isPausedProperty() {
+        return isPausedProperty.getReadOnlyProperty();
+    }
+
     public void setTimerLength(int timerLength) {
-        if (timerService.isRunning())
-            throw new IllegalStateException("Timer is currently running!");
         this.timerLength = timerLength;
-        updateLabel(timerLength);
     }
 
     public int getTimerLength() {
@@ -147,7 +120,7 @@ public class Timer extends VBox {
     }
 
     public boolean isRunning() {
-        return timerService.isRunning();
+        return isRunningProperty.get();
     }
 
     public interface TimerFinishedEventHandler {
