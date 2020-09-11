5cc8d9a33169efb86b5d3d2cd35f45e89169d099
Max <rudko.max10@gmail.com>
2020-09-11 17:59:15 +0300

    Generate fake data if the app uses mock implementation of result repository

--- src/com/justsoft/speedtyper/Main.java
+++ src/com/justsoft/speedtyper/Main.java
@@ -1,5 +1,8 @@
 package com.justsoft.speedtyper;
 
+import com.justsoft.speedtyper.model.TypingSessionResult;
+import com.justsoft.speedtyper.repositories.SessionResultsRepository;
+import com.justsoft.speedtyper.services.SessionResultMapService;
 import com.justsoft.speedtyper.ui.controllers.MainController;
 import com.justsoft.speedtyper.ui.dialogs.ExceptionDialog;
 import com.justsoft.speedtyper.util.Resources;
@@ -10,6 +13,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+
+import java.time.LocalDate;
 
 public class Main extends Application {
 
@@ -41,6 +46,24 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        createFakeData();
         launch(args);
+    }
+
+    private static void createFakeData() {
+        if (SessionResultsRepository.getPreferredInstance() instanceof SessionResultMapService) {
+            SessionResultsRepository.getPreferredInstance().save(
+                    new TypingSessionResult(240, 40, 2, LocalDate.of(2020, 2, 5), 60)
+            );
+            SessionResultsRepository.getPreferredInstance().save(
+                    new TypingSessionResult(200, 30, 5, LocalDate.of(2020, 2, 7), 60)
+            );
+            SessionResultsRepository.getPreferredInstance().save(
+                    new TypingSessionResult(220, 32, 1, LocalDate.of(2020, 2, 8), 60)
+            );
+            SessionResultsRepository.getPreferredInstance().save(
+                    new TypingSessionResult(280, 50, 0, LocalDate.of(2020, 2, 10), 60)
+            );
+        }
     }
 }
